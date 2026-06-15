0b2fb58d58395fbbf65e833d7d651e7f5958be62
Jackson Drenth <jackson.drenth@gmail.com>
2026-06-15 18:38:20 -0500

    added the ability to print a list of all your logs

--- til.py
+++ til.py
@@ -4,6 +4,7 @@ import click
 import json
 import datetime as dt
 import os
+from pprint import pprint
 
 @click.group()
 def cli():
@@ -39,6 +40,19 @@ def add(log, tag):
         json.dump(file_data, file, indent=4)
 
     click.echo("Notes logged!")
+
+@cli.command()
+def list():
+    # Handle missing or empty file safely
+    if os.path.exists("til.json") and os.path.getsize("til.json") > 0:
+        with open("til.json", "r") as file:
+            file_data = json.load(file)
+    else:
+        click.echo("You have no data in your TIL.json, maybe add something you learned today!")
+        pass
+    
+    if file_data:
+        pprint(file_data)
 
 if __name__ == "__main__":
     cli()
